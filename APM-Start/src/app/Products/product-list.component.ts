58231e0f0635daf2b68725d157caf405dbeadc74
Dorian <Dorian@github.com>
2018-01-13 00:07:19 -0400

    I have made both a search function and the template of my web tool

---
+++ APM-Start/src/app/Products/product-list.component.ts
@@ -0,0 +1,10 @@
+///<reference path="../../../node_modules/@angular/core/src/metadata/directives.d.ts"/>
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-products',
+  templateUrl: './product-list.component.html'
+})
+export class ProductListComponent{
+
+}
